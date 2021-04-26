6dc1eaf7d62d0de2fa6bee5dce40f8d8aa306714
myokyawthu129 <myokyawthu605@gmail.com>
2021-04-26 11:13:07 +0630

    Add votes, asnwers and views counter to question item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/prismjs-themes/prism.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-    
+
 </head>
 <body>
     <div id="app">
