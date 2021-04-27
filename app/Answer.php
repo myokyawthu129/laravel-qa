1a7fb39f51c0960509da24afd4b659b41da31b17
myokyawthu129 <myokyawthu605@gmail.com>
2021-04-27 11:49:24 +0630

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -19,4 +19,13 @@ class Answer extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function($answer) {
+           $answer->question->increment('answers_count');
+           $answer->question->save();
+        });
+      
+    }
 }
